dbc9864dfa92dfd1f69d9f23cbce9c98d42278c9
phaar <phaar@daq14>
2020-06-03 13:02:52 -0700

    Use actual NaN instead of placeholder number.

--- src/TemperatureSensor.cc
+++ src/TemperatureSensor.cc
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 #include <phidget22.h>
 #include <string>
 #include "Phidget.hh"
@@ -6,83 +7,83 @@
 
 TemperatureSensor::TemperatureSensor(int hub_port, int serialNumber) : PhidgetPP(hub_port, serialNumber)
 {
-  status = PhidgetTemperatureSensor_create(&handle);
-  phandle = (PhidgetHandle)handle;
+    status = PhidgetTemperatureSensor_create(&handle);
+    phandle = (PhidgetHandle)handle;
 
-  status = Phidget_setIsHubPortDevice(phandle, 0);
-  if (hubPort >= 0)
-    status = Phidget_setHubPort(phandle, hubPort);
-  if (serialNumber)
-    status = Phidget_setDeviceSerialNumber(phandle, serialNumber);
+    status = Phidget_setIsHubPortDevice(phandle, 0);
+    if (hubPort >= 0)
+        status = Phidget_setHubPort(phandle, hubPort);
+    if (serialNumber)
+        status = Phidget_setDeviceSerialNumber(phandle, serialNumber);
 
-  if (AllGood())
-  {
-    status = Phidget_openWaitForAttachment(phandle, 5000);
-  }
-  else
-  {
-    std::cerr << "Temperature Sensor Constructor Error:" << GetErrorCode() << std::endl;
-  }
+    if (AllGood())
+        {
+            status = Phidget_openWaitForAttachment(phandle, 5000);
+        }
+    else
+        {
+            std::cerr << "Temperature Sensor Constructor Error:" << GetErrorCode() << std::endl;
+        }
 }
 
 TemperatureSensor::~TemperatureSensor()
 {
-  /*!
-    * Destructor disconnects the device and delets the handle
-    */
-  Phidget_close(phandle);
-  PhidgetTemperatureSensor_delete(&handle);
+    /*!
+     * Destructor disconnects the device and delets the handle
+     */
+    Phidget_close(phandle);
+    PhidgetTemperatureSensor_delete(&handle);
 }
 
 double TemperatureSensor::GetTemp()
 {
-  /*!
-    *Returns the current temperature measured by the phidget
-    */
-  status = PhidgetTemperatureSensor_getTemperature(handle, &temperature);
-  if (status == EPHIDGET_OK)
-    return temperature;
-  else
-    return -999999.;
+    /*!
+     *Returns the current temperature measured by the phidget
+     */
+    status = PhidgetTemperatureSensor_getTemperature(handle, &temperature);
+    if (status == EPHIDGET_OK)
+        return temperature;
+    else
+        return std::numeric_limits<double>::quiet_NaN();
 }
 
 double TemperatureSensor::GetTempChangeTrigger()
 {
-  /*!
-    * Returns the minimum threshold to trigger a change in temperature event.
-    * Default value is 0.0 units
-    */
-  status = PhidgetTemperatureSensor_getTemperatureChangeTrigger(handle, &tempChangeTrigger);
-  return tempChangeTrigger;
+    /*!
+     * Returns the minimum threshold to trigger a change in temperature event.
+     * Default value is 0.0 units
+     */
+    status = PhidgetTemperatureSensor_getTemperatureChangeTrigger(handle, &tempChangeTrigger);
+    return tempChangeTrigger;
 }
 
 PhidgetReturnCode TemperatureSensor::SetTempChangeTrigger(double newTempChangeTrigger)
 {
-  /*!
-    * Sets the minimum threshold to trigger a change in temperature event.
-    * Default value is 0.0 units
-    */
-  status = PhidgetTemperatureSensor_getTemperatureChangeTrigger(handle, &newTempChangeTrigger);
-  return status;
+    /*!
+     * Sets the minimum threshold to trigger a change in temperature event.
+     * Default value is 0.0 units
+     */
+    status = PhidgetTemperatureSensor_getTemperatureChangeTrigger(handle, &newTempChangeTrigger);
+    return status;
 }
 
 PhidgetReturnCode TemperatureSensor::SetTempChangeFunc(PhidgetTemperatureSensor_OnTemperatureChangeCallback tempChangeFunc)
 {
-  /*!
-    * Sets the function that will be called when a temperature change event occurs
-    */
-  status = PhidgetTemperatureSensor_setOnTemperatureChangeHandler(handle, tempChangeFunc, NULL);
-  return status;
+    /*!
+     * Sets the function that will be called when a temperature change event occurs
+     */
+    status = PhidgetTemperatureSensor_setOnTemperatureChangeHandler(handle, tempChangeFunc, NULL);
+    return status;
 }
 
 double TemperatureSensor::GetMinTemp(){
-  double temp;
-  PhidgetTemperatureSensor_getMinTemperature(handle, &temp);
-  return temp;
+    double temp;
+    PhidgetTemperatureSensor_getMinTemperature(handle, &temp);
+    return temp;
 }
 
 double TemperatureSensor::GetMaxTemp(){
-  double temp;
-  PhidgetTemperatureSensor_getMaxTemperature(handle, &temp);
-  return temp;
+    double temp;
+    PhidgetTemperatureSensor_getMaxTemperature(handle, &temp);
+    return temp;
 }
--- src/VoltageInput.cc
+++ src/VoltageInput.cc
@@ -1,64 +1,67 @@
 #include <iostream>
 #include <cassert>
 #include <stdio.h>
+#include <limits>
 #include <phidget22.h>
 #include "Phidget.hh"
 #include "VoltageInput.hh"
 
 VoltageInput::VoltageInput(int hub_port, int serialNumber, bool hubPortDevice) : PhidgetPP(hub_port, serialNumber)
 {
-  status = PhidgetVoltageInput_create(&handle);
-  phandle = (PhidgetHandle)handle;
-  status = Phidget_setIsHubPortDevice(phandle, int(hubPortDevice));
-  if (hubPort >= 0)
-    status = Phidget_setHubPort(phandle, hubPort);
-  if (serialNumber)
-    status = Phidget_setDeviceSerialNumber(phandle, serialNumber);
-  if (AllGood())
-  {
-    status = Phidget_openWaitForAttachment(phandle, 5000);
-  }
-  else
-  {
-    std::cerr << "Voltage Input Constructor Error:" << GetErrorCode() << std::endl;
-  }
+    status = PhidgetVoltageInput_create(&handle);
+    phandle = (PhidgetHandle)handle;
+    status = Phidget_setIsHubPortDevice(phandle, int(hubPortDevice));
+    if (hubPort >= 0)
+        status = Phidget_setHubPort(phandle, hubPort);
+    if (serialNumber)
+        status = Phidget_setDeviceSerialNumber(phandle, serialNumber);
+    if (AllGood())
+        {
+            status = Phidget_openWaitForAttachment(phandle, 5000);
+        }
+    else
+        {
+            std::cerr << "Voltage Input Constructor Error:" << GetErrorCode() << std::endl;
+        }
 }
 
 VoltageInput::~VoltageInput()
 {
-  Phidget_close(phandle);
-  PhidgetVoltageInput_delete(&handle);
+    Phidget_close(phandle);
+    PhidgetVoltageInput_delete(&handle);
 }
 
 double VoltageInput::GetVoltage()
 {
-  status = PhidgetVoltageInput_getVoltage(handle, &voltage);
-  if (status == EPHIDGET_OK)
-    return voltage;
-  else
-    return -999999.;
+    status = PhidgetVoltageInput_getVoltage(handle, &voltage);
+    if (status == EPHIDGET_OK)
+        return voltage;
+    else
+        return std::numeric_limits<double>::quiet_NaN();
+
 }
 
 double VoltageInput::GetVoltageValueChangeTrigger()
 {
-  status = PhidgetVoltageInput_getVoltageChangeTrigger(handle, &valueChangeTrigger);
-  if (status == EPHIDGET_OK)
-    return valueChangeTrigger;
-  else
-    return -999999.;
+    status = PhidgetVoltageInput_getVoltageChangeTrigger(handle, &valueChangeTrigger);
+    if (status == EPHIDGET_OK)
+        return valueChangeTrigger;
+    else
+        return std::numeric_limits<double>::quiet_NaN();
+
 }
 
 int VoltageInput::SetVoltageValueChangeTrigger(double newValueChangeTrigger)
 {
-  status = PhidgetVoltageInput_setVoltageChangeTrigger(handle, newValueChangeTrigger);
-  if (status == EPHIDGET_OK)
-    return 1;
-  else
-    return 0.;
+    status = PhidgetVoltageInput_setVoltageChangeTrigger(handle, newValueChangeTrigger);
+    if (status == EPHIDGET_OK)
+        return 1;
+    else
+        return 0.;
 }
 
 PhidgetReturnCode VoltageInput::setVoltageValueChangeFunc(PhidgetVoltageInput_OnVoltageChangeCallback tempChangeFunc)
 {
-  status = PhidgetVoltageInput_setOnVoltageChangeHandler(handle, tempChangeFunc, NULL);
-  return status;
+    status = PhidgetVoltageInput_setOnVoltageChangeHandler(handle, tempChangeFunc, NULL);
+    return status;
 }
